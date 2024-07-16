b0d1e7a24c817edb748363bf2fe728f2c4d9e835
NataliaRozario <natyraissa137@gmail.com>
2024-07-16 00:02:35 -0300

    desafio 3

---
+++ ranking/index.js
@@ -0,0 +1,27 @@
+const vitorias = 105;
+const derrotas = 13;
+let nivel;
+
+const saldo = calculoSaldo(vitorias, derrotas);
+
+function calculoSaldo(vitorias, derrotas) {
+  return vitorias - derrotas;
+}
+
+if (saldo <= 10) {
+  nivel = "Ferro";
+} else if (saldo >= 11 && saldo <= 20) {
+  nivel = "Bronze";
+} else if (saldo >= 21 && saldo <= 50) {
+  nivel = "Prata";
+} else if (saldo >= 51 && saldo <= 80) {
+  nivel = "Ouro";
+} else if (saldo >= 81 && saldo <= 90) {
+  nivel = "Diamante";
+} else if (saldo >= 91 && saldo <= 100) {
+  nivel = "Lendário";
+} else {
+  nivel = "Imortal";
+}
+
+console.log(`O Herói tem saldo de ${saldo} está no nível de ${nivel}`);
